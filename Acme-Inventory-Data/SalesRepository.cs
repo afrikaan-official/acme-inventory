8c3f4215b9f5dfb1ee762195bfce0e53f9c0bef0
kaan yurdakok <kaan.yurdakok@accenture.com>
2019-09-20 15:41:50 +0300

    initial

---
+++ Acme-Inventory-Data/SalesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace Acme_Inventory_Data
+{
+    public class SalesRepository
+    {
+        public SalesRepository()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "db", "acme-inventory.db");
+            var bag = new SQLiteConnection($"Data Source = {path}; Version=3;", true);
+
+            bag.Open();
+
+            Console.WriteLine(bag.State);
+        }
+    }
+}
---
+++ Acme-Inventory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Acme_Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var restart = "";
+            do
+            {
+                //main part of program
+                Console.Write("Do you wish to calculate another? (YES/NO) ");
+                restart = Console.ReadLine().ToUpper();
+                while ((restart != "YES") && (restart != "NO")) //????
+                {
+                    Console.WriteLine("Error");
+                    Console.WriteLine("Do you wish to calculate another? (YES/NO) ");
+                    restart = Console.ReadLine().ToUpper();
+                }
+            } while (restart == "YES");
+        }
+    }
+}
